0feab262f806efdb8c23a0f8e0a0cbdd1529ce98
Anil Kumar Prajapat  :  ${ A.K.P } <111525796+ExterminatorA7@users.noreply.github.com>
2023-08-14 18:38:03 +0530

    Register API url change

--- src/api/axios-client.js
+++ src/api/axios-client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { ENVIRONMENT } from "../constants/environment"
 
-export const BASE_URL = ENVIRONMENT === 'production' ? '' : 'http://192.168.1.6:5000/api'
+export const BASE_URL = ENVIRONMENT === 'production' ? '' : 'http://13.53.40.209:5000/api'
 
 const axiosClient = () => {
     return axios.create({
--- src/screen/Auth/Register.js
+++ src/screen/Auth/Register.js
@@ -1,31 +1,29 @@
-import { View, Text, TouchableOpacity, Image, Dimensions, Alert } from 'react-native'
+import { View, Text, TouchableOpacity, Image, Dimensions, ActivityIndicator } from 'react-native'
 import React, { useState } from 'react'
 import { Layout } from '@ui-kitten/components'
 import { TextInput } from '@react-native-material/core'
 import Logo from '../../../assets/logo.png'
 import WhatsIcon from '../../../assets/whats-app.svg'
 import axiosClient from '../../api/axios-client'
-
+import Snackbar from 'react-native-snackbar';
 
 const win = Dimensions.get('window')
 const ratio = win.width / 541; //541 is actual image width
 
 const Register = ({ navigation }) => {
     const [name, setName] = useState('')
-    const [mobileNumber, setMobileNumber] = useState('')
     const [password, setPassword] = useState('')
+    const [mobileNumber, setMobileNumber] = useState('')
 
-    const [loding, setLoading] = useState(false)
-
-    // const { makeRequest } = useRequest()
+    const [loading, setLoading] = useState(false)
 
     const onPress = () => {
-
+        setLoading(true)
         axiosClient()
             .post('user/register', {
                 "firstName": name,
                 "lastName": "Kumar",
-                "email": "Sunil.Kumar@truworth.com",
+                "email": `${name}@gmail.com`,
                 "mobile": mobileNumber,
                 "password": password
             },
@@ -33,10 +31,21 @@ const Register = ({ navigation }) => {
                     headers: 'Content-Type: application/json',
                 }
             ).then(async res => {
-                const { } = res.data
-                console.log(res.data);
-            }).catch(err => {
-                Alert.alert('The Reddy Anna', err.response.data.message)
+                setLoading(false)
+                navigation.navigate('Login')
+                Snackbar.show({
+                    text: 'User Register Successfully',
+                    duration: Snackbar.LENGTH_SHORT,
+                });
+                setName('')
+                setMobileNumber('')
+                setPassword('')
+            }).catch(() => {
+                setLoading(false)
+                Snackbar.show({
+                    text: 'User Already Register',
+                    duration: Snackbar.LENGTH_SHORT,
+                })
             })
 
         // makeRequest({
@@ -60,92 +69,87 @@ const Register = ({ navigation }) => {
 
     return (
         <Layout style={{ flex: 1, backgroundColor: '#000' }}>
-            <View style={{ backgroundColor: '#000', padding: 20 }}>
 
-                <View style={{ paddingTop: 100, paddingBottom: 28, justifyContent: 'center', alignItems: 'center' }}>
-                    <Image source={Logo} style={{
-                        width: win.width,
-                        height: 100 * ratio, //362 is actual height of image
-                        resizeMode: 'contain',
-                    }} />
+            {loading ?
+                <ActivityIndicator size={'large'} style={{ flex: 1, justifyContent: 'center' }} />
+                :
+                <View style={{ backgroundColor: '#000', padding: 20 }}>
+
+                    <View style={{ paddingTop: 100, paddingBottom: 28, justifyContent: 'center', alignItems: 'center' }}>
+                        <Image source={Logo} style={{
+                            width: win.width,
+                            height: 100 * ratio, //362 is actual height of image
+                            resizeMode: 'contain',
+                        }} />
+                    </View>
+
+                    <Text style={{ fontSize: 18, color: '#d8b664', padding: 8 }}>Welcome Back Register</Text>
+
+                    <TextInput
+                        value={name}
+                        variant="standard"
+                        style={{ padding: 8 }}
+                        placeholder='Enter Name'
+                        placeholderTextColor={'#d8b664'}
+                        inputStyle={{ color: 'white' }}
+                        onChangeText={(value) => { setName(value) }}
+                    />
+
+                    <TextInput
+                        value={mobileNumber}
+                        variant="standard"
+                        style={{ padding: 8 }}
+                        placeholder='Enter Mobile Number'
+                        placeholderTextColor={'#d8b664'}
+                        inputStyle={{ color: 'white' }}
+                        onChangeText={(value) => { setMobileNumber(value) }}
+                    />
+
+                    <TextInput
+                        value={password}
+                        variant="standard"
+                        style={{ padding: 8 }}
+                        placeholder='Enter Password'
+                        placeholderTextColor={'#d8b664'}
+                        inputStyle={{ color: 'white' }}
+                        onChangeText={(value) => { setPassword(value) }}
+                    />
+
+                    <TouchableOpacity
+                        activeOpacity={0.8}
+                        style={{
+                            backgroundColor: '#d8b664',
+                            alignItems: 'center', padding: 12,
+                            borderRadius: 4, margin: 8
+                        }}
+                        disabled={(name.length < 0 && mobileNumber.length < 0 && password.length < 0)}
+                        onPress={() => onPress()}
+                    >
+                        <Text style={{ fontSize: 14, fontWeight: '400', color: '#000' }}>CONFIRM</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity
+                        activeOpacity={0.8}
+                        style={{ flexDirection: 'row', padding: 8 }}
+                        onPress={() => { navigation.navigate('Login') }}
+                    >
+                        <Text style={{ color: '#d8b664' }}>
+                            Already Register Account ?
+                        </Text>
+                        <Text style={{ color: 'white', marginStart: 8 }}>
+                            Login
+                        </Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity
+                        activeOpacity={0.8}
+                        style={{ backgroundColor: '#d8b664', alignItems: 'center', padding: 12, borderRadius: 4, margin: 8, flexDirection: 'row', justifyContent: 'center' }}
+                    >
+                        <WhatsIcon height={25} width={25} />
+                        <Text style={{ fontSize: 14, fontWeight: '400', color: '#000', marginLeft: 12 }}>WHATS APP</Text>
+                    </TouchableOpacity>
                 </View>
-
-                <Text style={{ fontSize: 18, color: '#d8b664', padding: 8 }}>Welcome Back Register</Text>
-
-                <TextInput
-                    // label="Enter Mobile Number"
-                    variant="standard"
-                    style={{ padding: 8 }}
-                    placeholder='Enter Name'
-                    placeholderTextColor={'#d8b664'}
-                    inputStyle={{ color: 'white' }}
-                />
-
-                <TextInput
-                    // label="Enter Mobile Number"
-                    variant="standard"
-                    style={{ padding: 8 }}
-                    placeholder='Enter Mobile Number'
-                    placeholderTextColor={'#d8b664'}
-                    inputStyle={{ color: 'white' }}
-                />
-
-                {
-                    /* <Input
-                    placeholder='Enter Mobile Number'
-                    style={{ padding: 8 }}
-                    /> */
-                }
-
-                <TextInput
-                    // label="Label"
-                    variant="standard"
-                    style={{ padding: 8 }}
-                    placeholder='Enter Password'
-                    placeholderTextColor={'#d8b664'}
-                    inputStyle={{ color: 'white' }}
-                />
-
-                {
-                    /* <Input
-                    placeholder='Enter Password'
-                    style={{ padding: 8 }}
-                    /> */
-                }
-
-                <TouchableOpacity
-                    activeOpacity={0.8}
-                    style={{
-                        backgroundColor: '#d8b664',
-                        alignItems: 'center', padding: 12,
-                        borderRadius: 4, margin: 8
-                    }}
-                    onPress={() => onPress()}
-                >
-                    <Text style={{ fontSize: 14, fontWeight: '400', color: '#000' }}>CONFIRM</Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity
-                    activeOpacity={0.8}
-                    style={{ flexDirection: 'row', padding: 8 }}
-                    onPress={() => { navigation.navigate('Login') }}
-                >
-                    <Text style={{ color: '#d8b664' }}>
-                        Already Register Account ?
-                    </Text>
-                    <Text style={{ color: 'white', marginStart: 8 }}>
-                        Login
-                    </Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity
-                    activeOpacity={0.8}
-                    style={{ backgroundColor: '#d8b664', alignItems: 'center', padding: 12, borderRadius: 4, margin: 8, flexDirection: 'row', justifyContent: 'center' }}
-                >
-                    <WhatsIcon height={25} width={25} />
-                    <Text style={{ fontSize: 14, fontWeight: '400', color: '#000', marginLeft: 12 }}>WHATS APP</Text>
-                </TouchableOpacity>
-            </View>
+            }
         </Layout>
     )
 }
