a22e87a0b1bb398d4af47e706115692e02f5e838
Anil Kumar Prajapat  :  ${ A.K.P } <111525796+ExterminatorA7@users.noreply.github.com>
2023-08-25 23:50:52 +0530

    update code

--- src/screen/Auth/Register.js
+++ src/screen/Auth/Register.js
@@ -1,11 +1,12 @@
-import { View, Text, TouchableOpacity, Image, Dimensions, ActivityIndicator } from 'react-native'
+import { View, Text, TouchableOpacity, Image, Dimensions, ActivityIndicator, Linking, Alert, ScrollView } from 'react-native'
 import React, { useState } from 'react'
 import { Layout } from '@ui-kitten/components'
-import { TextInput } from '@react-native-material/core'
+// import { TextInput } from '@react-native-material/core'
 import Logo from '../../../assets/logo.png'
 import WhatsIcon from '../../../assets/whats-app.svg'
 import axiosClient from '../../api/axios-client'
 import Snackbar from 'react-native-snackbar';
+import { Button, TextInput } from 'react-native-paper';
 
 const win = Dimensions.get('window')
 const ratio = win.width / 541; //541 is actual image width
@@ -16,8 +17,37 @@ const Register = ({ navigation }) => {
     const [mobileNumber, setMobileNumber] = useState('')
 
     const [loading, setLoading] = useState(false)
+    const [hasMobileErrors, setMobileErrors] = useState(false)
+    const [hasPasswordErrors, setPasswordErrors] = useState(false)
+    const [hasNameErrors, setNameErrors] = useState(false)
 
     const onPress = () => {
+        if (name.length === 0) {
+            Alert.alert('Reddy Anna', 'Please enter your name')
+            setNameErrors(!hasNameErrors)
+            return
+        }
+        if (mobileNumber.length === 0) {
+            Alert.alert('Reddy Anna', 'Please enter your register mobile number')
+            setMobileErrors(!hasMobileErrors)
+            return
+        }
+        if (mobileNumber.length !== 10) {
+            Snackbar.show({
+                text: 'Please insert 10 digit mobile number',
+                duration: Snackbar.LENGTH_SHORT,
+            })
+            setMobileErrors(true)
+            return
+        }
+        if (password.length === 0) {
+            Snackbar.show({
+                text: 'Please insert your password',
+                duration: Snackbar.LENGTH_SHORT,
+            })
+            setPasswordErrors(true)
+            return
+        }
         setLoading(true)
         axiosClient()
             .post('user/register', {
@@ -32,7 +62,7 @@ const Register = ({ navigation }) => {
                 }
             ).then(async res => {
                 setLoading(false)
-                navigation.navigate('Login')
+                navigation.navigate('Home')
                 Snackbar.show({
                     text: 'User Register Successfully',
                     duration: Snackbar.LENGTH_SHORT,
@@ -67,13 +97,37 @@ const Register = ({ navigation }) => {
         // })
     }
 
+    const clickOnWhatsAppBtn = () => {
+        // Check for perfect 10 digit length
+        // if (mobileNumber.length !== 10) {
+        //     Alert.alert('Please insert correct WhatsApp number');
+        //     return;
+        // }
+        // Using 91 for India
+        // You can change 91 with your country code
+        let url =
+            'whatsapp://send?text=' +
+            'I need id' +
+            '&phone=91' + '7733040777';
+        Linking.openURL(url)
+            .then((data) => {
+                console.log('WhatsApp Opened');
+            })
+            .catch(() => {
+                Alert.alert('Make sure Whatsapp installed on your device');
+            });
+    }
+
     return (
         <Layout style={{ flex: 1, backgroundColor: '#000' }}>
 
             {loading ?
                 <ActivityIndicator size={'large'} style={{ flex: 1, justifyContent: 'center' }} />
                 :
-                <View style={{ backgroundColor: '#000', padding: 20 }}>
+                <ScrollView
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={{ flexGrow: 1, padding: 16 }}
+                >
 
                     <View style={{ paddingTop: 100, paddingBottom: 28, justifyContent: 'center', alignItems: 'center' }}>
                         <Image source={Logo} style={{
@@ -86,16 +140,86 @@ const Register = ({ navigation }) => {
                     <Text style={{ fontSize: 18, color: '#d8b664', padding: 8 }}>Welcome Back Register</Text>
 
                     <TextInput
+                        // label=""
+                        mode="outlined"
                         value={name}
-                        variant="standard"
-                        style={{ padding: 8 }}
-                        placeholder='Enter Name'
+                        style={{
+                            padding: 5, backgroundColor: 'transparent',
+                            marginHorizontal: 8
+                        }}
+                        textColor='#d8b664'
+                        onChangeText={(e) => setName(e)}
+                        textContentType="telephoneNumber"
+                        returnKeyType="next"
+                        keyboardType="name-phone-pad"
+                        autoCapitalize="none"
+                        error={hasNameErrors}
+                        placeholder="Enter Name"
                         placeholderTextColor={'#d8b664'}
-                        inputStyle={{ color: 'white' }}
-                        onChangeText={(value) => { setName(value) }}
+                        clearTextOnFocus={true}
+                        // outlineColor='#d8b664'
+                        activeOutlineColor='#d8b664'
+                    />
+
+
+                    <TextInput
+                        // label=""
+                        mode="outlined"
+                        value={mobileNumber}
+                        style={{
+                            padding: 5, backgroundColor: 'transparent',
+                            marginHorizontal: 8, marginTop: 16
+                        }}
+                        textColor='#d8b664'
+                        onChangeText={(e) => setMobileNumber(e)}
+                        textContentType="telephoneNumber"
+                        returnKeyType="next"
+                        keyboardType="numeric"
+                        autoCapitalize="none"
+                        error={hasMobileErrors}
+                        placeholder="Enter Mobile Number"
+                        placeholderTextColor={'#d8b664'}
+                        clearTextOnFocus={true}
+                        // outlineColor='#d8b664'
+                        activeOutlineColor='#d8b664'
                     />
 
                     <TextInput
+                        // label=""
+                        mode="outlined"
+                        value={password}
+                        style={{
+                            padding: 5, backgroundColor: 'transparent',
+                            marginHorizontal: 8, marginTop: 16
+                        }}
+                        textColor='#d8b664'
+                        onChangeText={(e) => setPassword(e)}
+                        textContentType="telephoneNumber"
+                        returnKeyType="next"
+                        keyboardType="default"
+                        autoCapitalize="none"
+                        error={hasPasswordErrors}
+                        placeholder="Enter Password"
+                        placeholderTextColor={'#d8b664'}
+                        clearTextOnFocus={true}
+                        secureTextEntry={true}
+                        // outlineColor='#d8b664'
+                        activeOutlineColor='#d8b664'
+
+                    />
+
+
+                    {/* // <TextInput
+                    //     value={name}
+                    //     variant="standard"
+                    //     style={{ padding: 8 }}
+                    //     placeholder='Enter Name'
+                    //     placeholderTextColor={'#d8b664'}
+                    //     inputStyle={{ color: 'white' }}
+                    //     onChangeText={(value) => { setName(value) }}
+                    // /> */}
+
+                    {/* <TextInput
                         value={mobileNumber}
                         variant="standard"
                         style={{ padding: 8 }}
@@ -113,42 +237,52 @@ const Register = ({ navigation }) => {
                         placeholderTextColor={'#d8b664'}
                         inputStyle={{ color: 'white' }}
                         onChangeText={(value) => { setPassword(value) }}
-                    />
+                    /> */}
 
                     <TouchableOpacity
                         activeOpacity={0.8}
                         style={{
                             backgroundColor: '#d8b664',
-                            alignItems: 'center', padding: 12,
-                            borderRadius: 4, margin: 8
+                            alignItems: 'center', padding: 16,
+                            borderRadius: 4, marginHorizontal: 8, marginTop: 22
                         }}
                         disabled={(name.length < 0 && mobileNumber.length < 0 && password.length < 0)}
                         onPress={() => onPress()}
                     >
-                        <Text style={{ fontSize: 14, fontWeight: '400', color: '#000' }}>CONFIRM</Text>
+                        <Text style={{ fontSize: 14, fontWeight: '600', color: '#000' }}>CONFIRM</Text>
                     </TouchableOpacity>
 
                     <TouchableOpacity
                         activeOpacity={0.8}
-                        style={{ flexDirection: 'row', padding: 8 }}
+                        style={{ flexDirection: 'row', padding: 8, marginVertical: 8 }}
                         onPress={() => { navigation.navigate('Login') }}
                     >
-                        <Text style={{ color: '#d8b664' }}>
+                        <Text style={{ fontSize: 16, color: '#d8b664' }}>
                             Already Register Account ?
                         </Text>
-                        <Text style={{ color: 'white', marginStart: 8 }}>
+                        <Text style={{ fontSize: 16, color: 'white', marginStart: 8 }}>
                             Login
                         </Text>
                     </TouchableOpacity>
 
                     <TouchableOpacity
                         activeOpacity={0.8}
-                        style={{ backgroundColor: '#d8b664', alignItems: 'center', padding: 12, borderRadius: 4, margin: 8, flexDirection: 'row', justifyContent: 'center' }}
+                        style={{
+                            backgroundColor: '#d8b664', alignItems: 'center',
+                            padding: 16, borderRadius: 4, margin: 8,
+                            flexDirection: 'row', justifyContent: 'center'
+                        }}
+                        onPress={clickOnWhatsAppBtn}
                     >
                         <WhatsIcon height={25} width={25} />
-                        <Text style={{ fontSize: 14, fontWeight: '400', color: '#000', marginLeft: 12 }}>WHATS APP</Text>
+                        <Text style={{
+                            fontSize: 14, fontWeight: '600',
+                            color: '#000', marginLeft: 12
+                        }}>
+                            WHATS APP
+                        </Text>
                     </TouchableOpacity>
-                </View>
+                </ScrollView>
             }
         </Layout>
     )
--- src/screen/Home.js
+++ src/screen/Home.js
@@ -1,16 +1,52 @@
-import { View } from 'react-native'
+import { View, Image, Dimensions, Text, TouchableOpacity, Linking, Alert } from 'react-native'
 import React from 'react'
-import WebView from 'react-native-webview'
+import ImageHome from '../../assets/home.jpg'
+import { Button } from '@react-native-material/core'
+import WhatsIcon from '../../assets/whats-app.svg'
 
-const Home = () => {
+const win = Dimensions.get('window')
+const ratio = win.width / 541; //541 is actual image width
+
+const Home = ({ navigation }) => {
     return (
         <View style={{ flex: 1 }}>
-            <WebView
-                source={{
-                    uri: 'https://api.whatsapp.com/send?phone=9057921046',
+            <Image
+                source={ImageHome}
+                style={{
+                    width: '100%',
+                    height: '100%', //362 is actual height of image
                 }}
-                style={{ marginTop: 20 }}
             />
+
+            <TouchableOpacity
+                activeOpacity={0.8}
+                style={{
+                    position: 'absolute',
+                    right: 0, left: 0, bottom: '5%',
+                    borderRadius: 50, marginHorizontal: '10%', padding: 4,
+                    backgroundColor: 'white', justifyContent: 'center', alignItems: 'center'
+                }}
+                onPress={() => {
+                    // navigation.navigate('WebView')
+                    // let url = 'https://api.whatsapp.com/send?phone=7733040777'
+                    let url = 'https://api.whatsapp.com/send?text=' + 'I need id' + '&phone=91' + '7733040777';
+
+                    Linking.openURL(url)
+                        .then((data) => {
+                            console.log('WhatsApp Opened');
+                        })
+                        .catch(() => {
+                            Alert.alert('Make sure Whatsapp installed on your device');
+                        });
+                }}
+            >
+                <View style={{ flexDirection: 'row', margin: 8, alignItems: 'center' }}>
+                    <WhatsIcon height={25} width={25} />
+                    <Text style={{ fontSize: 16, fontWeight: 'bold', color: '#60d66a', marginLeft: 12, }}>
+                        CLICK HERE TO WHATSAPP & GET ID
+                    </Text>
+                </View>
+            </TouchableOpacity>
         </View>
     )
 }
